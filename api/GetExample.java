af062fdb1342a387d65f88af5b1831661bb40912
ReshmaBhaskar <83629070+ReshmaBhaskar@users.noreply.github.com>
2021-05-17 11:55:36 +0530

    Add files via upload

---
+++ api/GetExample.java
@@ -0,0 +1,40 @@
+package RESTExamples;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+
+public class GetExample {
+	
+	
+	// Base URL
+	String ROOT_URI = "https://petstore.swagger.io/v2/pet/";
+
+	//Test case using a DataProvider
+	@Test(dataProvider = "petIdProvider")
+	public void simple_get_test(String id) 
+	{
+	    Response response =  
+	    	given().contentType(ContentType.JSON) // Set headers
+	        .when().get(ROOT_URI + id); // Send GET request
+
+	    // Print response
+	    System.out.println(response.getBody().asPrettyString());
+	    // Assertions
+	    response.then().body("status", equalTo("alive"));
+	}
+
+	@DataProvider
+	public Object[][] petIdProvider()
+	{
+	    // Setting parameters to pass to test case
+	    Object[][] testData = new Object[][] { { "77232" }, { "77233" } };
+	    return testData;
+	}
+
+}
